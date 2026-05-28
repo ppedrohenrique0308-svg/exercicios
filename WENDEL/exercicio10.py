92a9a3f589aa83b5eb68e6590cd8250a33b22f51
Pedro Henrique <ppedrohenrique0308@gmail.com>
2026-05-28 15:36:11 -0300

    arrumando condiçoes do imc

--- WENDEL/exercicio10.py
+++ WENDEL/exercicio10.py
@@ -2,11 +2,11 @@ peso = int(input('peso: '))
 altura = float(input('altura: '))
 imc = peso / altura**2
 print(f'o seu imc é {imc:.2f}')
-if imc <= 18.5 :
+if imc < 18.5 :
     print('abaixo do peso')
-elif imc > 18.5 < 24.9:
+elif 18.5 <= imc <= 24.9:
     print('peso normal')
-elif 25 < imc < 29.9:
+elif 25 <= imc <= 29.9:
     print('sobre peso')
 else:
     print('obesidade')
